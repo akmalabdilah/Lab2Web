e61d00bb5506eeb0e8e868b549ba727115c187f1
M. AKMAL AL ABDILAH <92352562+akmalabdilah@users.noreply.github.com>
2023-03-14 15:59:07 +0700

    menambahkan file coding/

--- coding/coding3.php
+++ coding/coding3.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <body>
     <h1>Predevine Variable</h1>
     <?php
--- coding/from.php
+++ coding/from.php
@@ -14,7 +14,7 @@
         <input type="submit" value="Kirim">
     </form>
     <?php
-    echo 'Selamat Datang ' . $_GET['nama'];
+    echo 'Selamat Datang ' . $_POST['nama'];
     ?>
 </body>
 
